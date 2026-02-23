af228d29390ed978ee20e343610722088d8f5640
Elnur <elnurgasanovv07@gmail.com>
2026-02-24 02:14:36 +0400

    Add UpdateTaskItemDto class for task updates

---
+++ UpdateTaskItemDto.cs
@@ -0,0 +1,9 @@
+namespace ASP_NET_08._TaskFlow_DTOs.DTOs.TaskItem_DTOs;
+
+
+public class UpdateTaskItemDto
+{
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public Models.TaskStatus Status { get; set; } = Models.TaskStatus.ToDo;
+}

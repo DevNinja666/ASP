cd842ef1f690d86d9087040bcf7d035740b19f56
Elnur <elnurgasanovv07@gmail.com>
2026-02-25 23:30:51 +0400

    Create ContactViewModel.cs

---
+++ Models/ContactViewModel.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+namespace PortfolioMVC.Models
+{
+    public class ContactViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
